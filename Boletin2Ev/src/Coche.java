3dac6cc7f2824694b08286cc58245317b9777f9b
FP <FP@PC55>
2020-02-05 14:23:08 +0100

    Boletin 3

--- Boletin2Ev/src/Coche.java
+++ Boletin2Ev/src/Coche.java
@@ -1,4 +1,10 @@
 public class Coche extends Vehiculo {
+	
+	//metodos
+	public void mostrarDatos() {
+		
+		System.out.println(numeroPlazas);
+	}
 
 	private int numeroPlazas;
 	public Coche(String matricula, String tipo, int velocidadMaxima, int numeroPlazas) {
